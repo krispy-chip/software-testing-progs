c4693b51494910e371d8296645c6f5aee9fa04c2
SURRRRAAAAAJ <116282102+krispy-chip@users.noreply.github.com>
2024-07-14 17:05:44 +0530

    Update program3.c

--- program3.c
+++ program3.c
@@ -1,67 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    int locks, stocks, barrels, t_sales;
-    float commission;
+int main(){
+    int locks,stocks,barrels;
+    float comission=0;
+    int t_sales;
 
-    while(1) {
-        // Prompt for the total number of locks
-        printf("Enter the total number of locks (-1 to end):\n");
-        scanf("%d", &locks);
+    while(1){
+        printf("Enter the value of locks (-1 to end program):\n");
+    scanf("%d",&locks);
 
-        // Check for end condition
-        if (locks == -1) {
-            printf("Program ends\n");
-            break;
-        }
+    if(locks==-1){
+        printf("Program ends\n");
+        exit(0);
+    }
+    if(locks<=0 || locks>70)
+    {
+        printf("out of range : range(1-70)\n");
+        continue;
+    }
+    printf("Enter the value of stocks:\n");
+    scanf("%d",&stocks);
 
-        // Validate locks input
-        if ((locks <= 0) || (locks > 70)) {
-            printf("Values of locks not in range of 1...70\n");
-            continue;
-        }
 
-        // Prompt for the total number of stocks
-        printf("Enter the total number of stocks:\n");
-        scanf("%d", &stocks);
+    if(stocks<=0 || stocks>80)
+    {
+        printf("out of range : range(1-80)\n");
+        continue;
+    }
+    printf("Enter the value of barrels:\n");
+    scanf("%d",&barrels);
 
-        // Validate stocks input
-        if ((stocks <= 0) || (stocks > 80)) {
-            printf("Values of stocks not in range of 1...80\n");
-            continue;
-        }
 
-        // Prompt for the total number of barrels
-        printf("Enter the total number of barrels:\n");
-        scanf("%d", &barrels);
+    if(barrels<=0 || barrels>90)
+    {
+        printf("out of range : range(1-90)\n");
+        continue;
+    }
+    printf("locks : %d\nstocks : %d\nbarrels : %d\n",locks,stocks,barrels);
 
-        // Validate barrels input
-        if ((barrels <= 0) || (barrels > 90)) {
-            printf("Values of barrels not in range of 1...90\n");
-            continue;
-        }
+    t_sales = (locks*45)+(stocks*30)+(barrels*25);
+    printf("Total Sales is %d\n",t_sales);
 
-        // Calculate total sales value
-        t_sales = (locks * 45) + (stocks * 30) + (barrels * 25);
-
-        // Calculate commission based on total sales
-        if (t_sales <= 1000) {
-            commission = 0.10 * t_sales;
-        } else if (t_sales < 1800) {
-            commission = 0.10 * 1000;
-            commission += 0.15 * (t_sales - 1000);
-        } else {
-            commission = 0.10 * 1000;
-            commission += 0.15 * 800;
-            commission += 0.20 * (t_sales - 1800);
-        }
-
-        // Display the total sales and commission
-        printf("The total sales is %d\n", t_sales);
-        printf("The commission is %.2f\n", commission);
+    if(t_sales<=1000){
+        comission = 0.10*t_sales;
+    }
+    else if(t_sales<1800){
+        comission = 0.10*1000;
+        comission = comission + (0.15 * (t_sales - 1000));
+    }
+    else {
+        comission = 0.10*1000;
+        comission = comission + (0.15 * 800);
+        comission = comission + (0.20 * (t_sales - 1800));
     }
 
-    return 0;
+    printf("Total comission is : %.2f\n\n\n\n\n",comission);
+
+    }
 }
